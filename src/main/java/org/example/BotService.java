a7af91a4f3b9f688a8f1dc339f9cb0a8e7fdb494
Hakimjon <normatovhakimjon07@gmail.com>
2024-02-14 20:08:34 +0500

    add language

--- src/main/java/org/example/BotService.java
+++ src/main/java/org/example/BotService.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BotService {
-
     public SendMessage Salom(Long chatId){
         SendMessage sendMessage = new SendMessage();
         sendMessage.setText("Assalomu aleykum \n Botga xush kelibsiz☺");
@@ -26,18 +25,16 @@ public class BotService {
         List<KeyboardRow>rowList=new ArrayList<>();
         KeyboardRow row=new KeyboardRow();
         KeyboardButton button = new KeyboardButton();
-        button.setText("O`zbek tili");
+        KeyboardButton button1 = new KeyboardButton();
+        button.setText("O`zbek tili\uD83C\uDDFA\uD83C\uDDFF");
+        button1.setText("Русский язык\uD83C\uDDF7\uD83C\uDDFA");
         row.add(button);
+        row.add(button1);
         rowList.add(row);
 
         replyKeyboardMarkup.setKeyboard(rowList);
         replyKeyboardMarkup.setResizeKeyboard(true);
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
         return sendMessage;
     }
-
-
-
-
-
-
 }
