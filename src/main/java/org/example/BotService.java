30976ab885551087e609cd545ddfabd44725bb5c
Hakimjon <normatovhakimjon07@gmail.com>
2024-02-14 19:51:39 +0500

    add language

--- src/main/java/org/example/BotService.java
+++ src/main/java/org/example/BotService.java
@@ -1,6 +1,12 @@
 package org.example;
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BotService {
 
@@ -10,6 +16,25 @@ public class BotService {
         sendMessage.setChatId(chatId);
         return sendMessage;
     }
+
+    public SendMessage Til(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Tilni Tanlang \n Выбирайте язык");
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        List<KeyboardRow>rowList=new ArrayList<>();
+        KeyboardRow row=new KeyboardRow();
+        KeyboardButton button = new KeyboardButton();
+        button.setText("O`zbek tili");
+        row.add(button);
+        rowList.add(row);
+
+        replyKeyboardMarkup.setKeyboard(rowList);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        return sendMessage;
+    }
+
 
 
 
--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -3,17 +3,24 @@ package org.example;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 
 public class MyBot extends TelegramLongPollingBot {
+    BotService botservice=new BotService();
     @Override
     public void onUpdateReceived(Update update) {
 
         if (update.hasMessage() && update.getMessage().hasText()){
             String text = update.getMessage().getText();
             Long chatId = update.getMessage().getChatId();
-
             if (text.equals("/start")){
-
+                try {
+                    execute(botservice.Salom(chatId));
+                    execute(botservice.Til(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
             }
 
 
